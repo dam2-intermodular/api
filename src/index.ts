f74907272b325d62d33389a230ca5f626108f940
Victor Garcia <victorgf2011@gmail.com>
2024-02-09 17:32:14 +0100

    feat: serve images

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import mongo from "./mongo";
 import { HonoBase } from "hono/hono-base";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
+import { stream, streamText, streamSSE } from "hono/streaming";
+import path from "node:path";
 
 async function bootstrapMongo() {
   try {
@@ -17,11 +19,43 @@ async function bootstrapMongo() {
   }
 }
 
+function sanitizePath(path: string) {
+  return path.replace(/(\.\.\/|\/\.\.)/g, "");
+}
+
+function declarePublicDirectory(app: HonoBase) {
+  app.get("/public/*", async (c) => {
+    const requestedPath = new URL(c.req.url?.replace("/public", "")).pathname;
+    let localPath = path.join(process.cwd(), "public", requestedPath);
+    // HMM revisar
+    localPath = sanitizePath(localPath);
+
+    const file = Bun.file(localPath);
+    if (!(await file.exists())) {
+      return c.json({ error: "Not found" }, 404);
+    }
+
+    const fileStream = file.stream();
+    if (fileStream === null) {
+      return c.json({ error: "Not found" }, 404);
+    }
+
+    return stream(c, async (stream) => {
+      stream.onAbort(() => {
+        console.log("Aborted!");
+      });
+
+      await stream.pipe(fileStream);
+    });
+  });
+}
+
 export async function createApp(): Promise<HonoBase> {
   await bootstrapMongo();
 
   let app = new OpenAPIHono();
   declareRoutes(app);
+  declarePublicDirectory(app);
 
   app.get(
     "/docs",
